90bed99bf99a3ed4e801f131c4841d67b4d1e058
flaviozorzetto <flaviozorzetto11@gmail.com>
2022-04-25 18:19:40 -0300

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import './styles/css/styles.css';
 
 export default class App extends React.Component {
    constructor(props) {
